cdf6be47af1ce0887925e7658782a7bea5fcf8d7
Luc MOMAL <luc.momal@gmail.com>
2020-04-30 08:33:24 +0200

    fix heroku bis

--- src/index.ts
+++ src/index.ts
@@ -78,4 +78,4 @@ const server = new ApolloServer({
   tracing: true,
   formatError: err => new Error(err.message),
 });
-server.listen().then(() => console.log('Server is running on localhost:4000'));
+server.listen({ port: process.env.PORT || 4000 }).then(() => console.log('Server is running on localhost:4000'));
